189dce1ea7975e0920919313b9b1cda31d75fde8
Linzp <sunandmoon001@163.com>
2025-11-07 18:47:19 +0800

    升级版本

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:8040',
-            changeOrigin: true
-        })
-    );
+module.exports = function(app) {
+  app.use('/api', createProxyMiddleware({
+    target: 'http://localhost:8047', changeOrigin: true
+  }));
 };
